03b982f35dc702705b0b8ce6ac598c31af20726f
i_vvvy <ivvvy.e@gmail.com>
2022-12-18 22:32:37 +0900

    비주얼 디자인 및 슬라이드

---
+++ js/visual-carousels.js
@@ -0,0 +1,15 @@
+$(function () {
+  $('.slider-list').slick({
+    arrows: false,
+    fade: true,
+    dots: true,
+    autoplay: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    autoplaySpeed: 5000,
+    customPaging: function (slider, i) {
+      var tit = $(slider.$slides[i]).find('.dot').html();
+      return '<div class="pager-tit" class= "+ i +">' + tit + '</div>';
+    },
+  });
+});
