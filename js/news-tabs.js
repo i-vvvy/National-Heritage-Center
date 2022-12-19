0479a12390c025b485e34c24c85f8db48d848a8a
i_vvvy <ivvvy.e@gmail.com>
2022-12-19 23:50:08 +0900

    천연기념물센터 소식 레이아웃

---
+++ js/news-tabs.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.news-tabs li').click(function () {
+    var tab_id = $(this).attr('data-tab');
+
+    $('.news-tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $('#' + tab_id).addClass('current');
+  });
+});
